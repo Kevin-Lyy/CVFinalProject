26fa4dcc10862a782ed642b72fbdb217a87fd2c2
KevinLyy <kevin@KevinsOmen.localdomain>
2022-05-07 15:45:19 -0400

    tripped out

--- mosaic.py
+++ mosaic.py
@@ -1,15 +1,69 @@
 import cv2
 import numpy as np
 import imutils
+import PIL
 from PIL import Image
 import matplotlib.pyplot as plt
+from PIL import ImageColor
+
+Lego_colors = []
+def LegoDatabase():
+    legos_colors = open('LegoColors.txt','r')
+    colors = legos_colors.readlines()
+    for color in colors:
+        color = "#" + color.strip()
+        color = ImageColor.getcolor(color,"RGB")
+        Lego_colors.append(color)
 
 def convertToPixels(image,size):
     img = Image.open("threshold.jpg")
     pixelated = img.resize(size,Image.BILINEAR)
     sizeUp = pixelated.resize(img.size,Image.NEAREST)
-    sizeUp.save("clean.jpg")
+    sizeUp.save("mosaic.jpg")
+
+def delta_color(pixel):
+    red = []
+    green = []
+    blue = []
+    RGB = []
+    for std_lego_color in Lego_colors:
+        #if pixel[0] < std_lego_color[0] + 5 and pixel[0]  > std_lego_color[0] - 5 and pixel[1] < std_lego_color[1] + 5 and pixel[1]  > std_lego_color[1] - 5 and pixel[2] < std_lego_color[2] + 5 and pixel[2]  > std_lego_color[2] - 5:
+        Rcolor = abs(pixel[0] - std_lego_color[0])
+        red.append(Rcolor)
+        Gcolor = abs(pixel[1] - std_lego_color[1])
+        green.append(Gcolor)
+        Bcolor = abs(pixel[2] - std_lego_color[2])
+        blue.append(Bcolor)
+        RGB.append(Rcolor+Gcolor+Bcolor)
+    closest = min(RGB)
+    return (red[closest],green[closest],blue[closest])
 
 
-convertToPixels(image="threshold.jpg",size=(50,50))
+def reColorLego():
+    img = Image.open("mosaic.jpg")
+    img = img.convert("RGB")
+    colors = img.getdata()
+    new_image = []
+    for pixel in colors:
+        closest_color = delta_color(pixel)
+        new_image.append(closest_color)
+
+            
+            
+    img.putdata(new_image)
+    img.save("reColor.jpg")
+
+            
+
+            
+
+
+
+LegoDatabase()
+
+convertToPixels(image="threshold.jpg",size=(70,70))
+
+reColorLego()
+
+
 
